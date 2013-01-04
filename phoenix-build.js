3c56f0117ee68117d6ea58692f993879a3debdbe
kpdecker <kpdecker@gmail.com>
2013-01-04 00:22:57 -0600

    Build module

--- phoenix-build.js
+++ phoenix-build.js
@@ -1,69 +1,20 @@
 /*global jake */
-var child_process = require('child_process'),
-    fs = require('fs'),
+var build = require('./lib/build'),
     glob = require('glob'),
-    growl = require('growl'),
     packageGenerator = require('./lib/package-generator'),
-    portscanner = require('portscanner'),
     server = require('./lib/server'),
-    util = require('./lib/util'),
-      mkdir = util.mkdir,
-      streamData = util.streamData,
+    testRunner = require('./lib/test-runner'),
     wrench = require('wrench');
 
 exports.build = function(options) {
-  var complete = options.complete;
-
-  mkdir(options.dir);
-
-  var args = [
-    __dirname + '/node_modules/lumbar/bin/lumbar',
-    options.watch ? 'watch' : 'build',
-    '--config', options.configFile || './config/dev.json',
-    '--use', __dirname + '/node_modules/lumbar-long-expires',
-    '--use', __dirname + '/node_modules/lumbar-tester',
-    '--with', '{"includeTests": ' + !options.removeTests + '}',
-    '--use', __dirname + '/node_modules/lumbar-style-doc',
-    '--with', '{"removeDocs": ' + !!options.removeTests + '}',
-    '--use', 'conditional',
-    '--with', '{"env": "' + (options.minimize ? 'production' : 'dev') + '"}',
-    exports.lumbarFile || './lumbar.json',
-    (options.dir || '.')
-  ];
-  if (options.package) {
-    args.push('--package');
-    args.push(options.package);
-  }
-  if (options.minimize) {
-    args.push('--minimize');
-  }
-  if (options.sourceMap) {
-    args.push('--sourceMap');
-  }
-
-  var lumbar = child_process.spawn('node', args);
-  lumbar.stdout.on('data', function (data) {
-    streamData('lumbar: ', data);
-  });
-  lumbar.stderr.on('data', function (data) {
-    streamData('lumbar err: ', data);
-  });
-
-  lumbar.on('exit', function(code) {
-    if (!complete) {
-      growl('Lumbar Borked', { title: 'Lumbar Borked', sticky: true });
-    } else {
-      complete(code);
-    }
-  });
-  return lumbar;
+  options.lumbarFile = options.lumbarFile || exports.lumbarFile;
+  build.build(options);
 };
 exports.startServer = function(options) {
   options.projectDir = exports.projectDir;
   options.forceCORS = exports.forceCORS;
   return server.start(options);
 };
-
 exports.watch = function(server, mocks) {
   glob.sync('build/dev').forEach(wrench.rmdirSyncRecursive);
 
