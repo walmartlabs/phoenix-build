0be7d24cc05fa07ac400e83666196e7de677c888
kpdecker <kpdecker@gmail.com>
2013-01-03 23:59:34 -0600

    Server module

--- phoenix-build.js
+++ phoenix-build.js
@@ -5,6 +5,7 @@ var child_process = require('child_process'),
     growl = require('growl'),
     packageGenerator = require('./lib/package-generator'),
     portscanner = require('portscanner'),
+    server = require('./lib/server'),
     util = require('./lib/util'),
       mkdir = util.mkdir,
       streamData = util.streamData,
@@ -57,64 +58,10 @@ exports.build = function(options) {
   });
   return lumbar;
 };
-
 exports.startServer = function(options) {
-  var test = !!options.test,
-      onData = options.data,
-      complete = options.complete;
-
-  if (options.mocks) {
-    process.env.ENABLE_MOCKS = options.mocks;
-  }
-  var args = [
-    __dirname + '/node_modules/mock-server/bin/mock-server',
-    exports.projectDir,
-    '--proxy-server=' + options.proxy,
-    '--port=' + (options.port || 8080),
-    '--secure-port=' + (options.securePort || 8081)
-  ];
-  if (exports.forceCORS) {
-    args.push('--force-cors');
-  }
-
-  var server = child_process.spawn('node', args, {
-    env: process.env
-  });
-  server.stdout.on('data', function (data) {
-    if (onData) {
-      onData(data);
-    }
-
-    // Do not output module, etc requests if in test mode
-    if (!test || !/GET \/r\/phoenix/.test(data)) {
-      streamData((test ? '  ' : '') + 'server: ', data);
-    }
-  });
-  server.stderr.on('data', function (data) {
-    if (onData) {
-      onData(data);
-    }
-    streamData((test ? '  ' : '') + 'server: ', data);
-  });
-
-  if (!test) {
-    server.on('exit', function(code) {
-      growl('Server Borked', { title: 'Server Borked', sticky: true });
-
-      if (complete) {
-        complete(code);
-      }
-    });
-  }
-
-  // If we die we're taking the server with us (to prevent ports from hanging around)
-  process.on('exit', function() {
-    if (server) {
-      server.kill();
-    }
-  });
-
-  return server;
+  options.projectDir = exports.projectDir;
+  options.forceCORS = exports.forceCORS;
+  return server.start(options);
 };
 
 exports.watch = function(server, mocks) {
