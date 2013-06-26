8e2276795b28e6dda02c17e945fe2e7fc5abd28e
kpdecker <kpdecker@gmail.com>
2013-06-26 08:48:41 -0700

    Pass platform to the config method

--- test/run-css.js
+++ test/run-css.js
@@ -10,13 +10,13 @@ phantom.injectJs(phantom.casperPath + '/bin/bootstrap.js');
 var css = require(phantom.libPath + '/phantomcss.js'),
     test = require('./test/css-test.js');
 
-var config = test.config(),
-
-    port = phantom.args[0],
+var port = phantom.args[0],
     securePort = phantom.args[1],
     platform = phantom.args[2],
     userAgent = phantom.args[3],
     dirname = platform + (/Android/.test(userAgent) ? '_android' : '_ios')
+
+    config = test.config(platform),
     width = config.width || 320,
     height = config.height || 480;
 
