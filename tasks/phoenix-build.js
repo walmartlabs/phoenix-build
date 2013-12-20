c8c801719088db7963dae4d382b81e9d0d160c09
kpdecker <kpdecker@gmail.com>
2013-12-19 21:10:58 -0600

    Implement phoenix-build lumbar task

---
+++ tasks/phoenix-build.js
@@ -0,0 +1,41 @@
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('lumbar');
+
+  grunt.registerMultiTask('phoenix-build', 'outputs a projects module map', function() {
+    var buildDir = 'build/dev',
+        nodeModules = __dirname + '/../node_modules/';
+
+    var options = this.options({
+      configFile: './config/dev.json',
+      lumbarFile: './lumbar.json',
+      minimize: false,
+      output: './build',
+      removeTests: false,
+      verbose: false
+    });
+
+    var taskName = this.nameArgs.replace(/:/g, '_');
+    grunt.config('lumbar.' + taskName, {
+      background: false,
+      build: options.build && options.lumbarFile,
+      watch: options.watch && options.lumbarFile,
+      config: options.configFile,
+      libraries: [
+        __dirname + '/../mixin'
+      ],
+      minimize: options.minimize,
+      output: options.output,
+      package: options.package,
+      plugins: [
+        {use: nodeModules + 'lumbar-long-expires'},
+        {use: nodeModules + 'lumbar-tester', withJson: {includeTests: !options.removeTests}},
+        {use: nodeModules + 'lumbar-style-doc', withJson: {removeDocs: options.removeTests}},
+        {use: 'conditional', withJson: {env: options.minimize ? 'production' : 'dev'}}
+      ],
+      sourceMap: options.sourceMap,
+      verbose: options.verbose
+    });
+    grunt.task.run('lumbar:' + taskName);
+  });
+};
